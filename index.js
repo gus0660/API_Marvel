abc03ff05ef083871b383890dc464a8377010396
gus0660 <guillaumeroch3@gmail.com>
2024-01-16 16:05:03 +0100

    creation de fonctions et de boucle for affichage du dropdown

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-let url = `http://gateway.marvel.com/v1/public/comics?ts=1&apikey=bdd8927cc325728869e4e301c9b2592c&hash=fa57ecb68368db4b9a073022eb1020b1`;
+
+
+function creerBalise(element) {
+  return document.createElement(element);
+}
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+// on commence par établir la variable qui est l'url de l'API choisie
+let url = `http://gateway.marvel.com/v1/public/characters?ts=1&apikey=bdd8927cc325728869e4e301c9b2592c&hash=fa57ecb68368db4b9a073022eb1020b1`;
 
 fetch(url)
-  .then((response) => response.json())
-  .then(response => console.log(response.data.results))
+  .then(response => response.json())
+  .then(response => affichPersos(response))
+  
   .catch((err) => console.log("Erreur : " + err));
 
-  
+function affichPersos(response) {
+  let body = document.body;
+  let marvels = response.data.results;
+
+  let selectM = creerBalise("select");
+   for (let i = 1; i <  marvels.length; i++){
+  let optionM = creerBalise("option");
+  optionM.innerHTML = marvels[i].name;
+  append(selectM, optionM);
+   }
+   append(body, selectM);
+}
